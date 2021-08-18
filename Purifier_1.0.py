e976a13cd1f0a5bb971a6116a6f0129fa6b640be
Samuel Davidson <123samueld@gmail.com>
2021-08-18 11:51:34 +0100

    Finish file opener func

--- Purifier_1.0.py
+++ Purifier_1.0.py
@@ -1,26 +1,33 @@
-def generate_possible_names_of_files():
-    number_of_year = 2017
+def open_files_in_sequence():
+    number_of_year = 17
     number_of_month = 00
     number_of_day = 00
+    folder_address_prefix = "Test_Data_Folder/"
     
     for iterate_through_years in range(0,2):
         number_of_year = number_of_year + 1
         number_of_month = 00
-        
+        folder_address_builder_pt_I = str(number_of_year)
+
         for iterate_through_months in range(0, 12):
             number_of_month = number_of_month + 1
-            file_name_with_month_and_year = str(number_of_month)+"."+str(number_of_year)
-            filled_file_name_with_month_and_year =  file_name_with_month_and_year.zfill(7)
             number_of_day = 00
+            folder_address_builder_pt_II = str(number_of_month).zfill(2) + "." + folder_address_builder_pt_I
 
             for iterate_through_days in range(0,31):
                 number_of_day = number_of_day + 1
-                file_name_with_day_month_and_year = str(number_of_day)+"."+filled_file_name_with_month_and_year
-                filled_file_name_with_day_month_and_year = file_name_with_day_month_and_year.zfill(10)
-
-                file_name = filled_file_name_with_day_month_and_year
+                folder_address_builder_pt_III =  str(number_of_day).zfill(2) + "." + folder_address_builder_pt_II
+                folder_address = folder_address_prefix + folder_address_builder_pt_I + "/" + folder_address_builder_pt_II + "/" + folder_address_builder_pt_III + "/ibiza.txt"
                 
-    find_valid_matches_for_suggested_file_names(file_name)
+                try:
+                    if(open (folder_address, "r", encoding="utf-8-sig")):
+                        print("P" + folder_address)
+                        print("True")
+                except:
+                    print("False")
+                    print(folder_address)
+                
+    #find_valid_matches_for_suggested_file_names(file_name)
     
     #loop_through_files(file_name)
 
