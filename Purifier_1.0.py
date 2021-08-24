ab469d673d308e2862a3e02a97701be503116a13
Samuel Davidson <123samueld@gmail.com>
2021-08-24 14:51:48 +0100

    Finds only first name in each file, not the rest

--- Purifier_1.0.py
+++ Purifier_1.0.py
@@ -40,14 +40,21 @@ def extract_raw_data_from_files(file_name):
 def identify_valuable_file_data(raw_file_data):
     #Find hostel name
     start_of_name_search_term = "ShowAll=1"
-    length_of_search_term =len(start_of_name_search_term)
+    end_of_name_search_term = "https://"
+    length_of_start_search_term =len(start_of_name_search_term)
+    length_of_end_search_term =len(end_of_name_search_term)
     starting_place_counter = 0
+    ending_place_counter = 125
     for i in range(0,len(raw_file_data)):
-        place =  raw_file_data.index(start_of_name_search_term, starting_place_counter)
-        end_place = place+ 50
-        name = raw_file_data[place+11: end_place]
+        start_place =  raw_file_data.index(start_of_name_search_term, starting_place_counter) + 12
+        end_place = raw_file_data.index(end_of_name_search_term, ending_place_counter) - 3
+        #length_of_name = end_place - start_place
+        name = raw_file_data[start_place:end_place]
         print("The name of the hostel is: "  + name)
-        starting_place_counter = place + length_of_search_term
+        print(start_place)
+        print(end_place)
+        starting_place_counter = start_place + length_of_start_search_term
+        ending_place_counter = end_place + length_of_end_search_term
 
 
 
